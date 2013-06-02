d745af1db467bc67d31d3c98c9b52c3b834bfad7
Wiktor <wuies@op.pl>
2013-06-03 01:30:35 +0200

    Wiadomość - modyfikacja

--- wiadomosc.py
+++ wiadomosc.py
@@ -4,20 +4,15 @@ class message:
         self.receiver = "default_receiver"
         self.sender = "default_sender"
         self.content = "default_content"
-        self.packet = "default_packet"
 
-    def DividePacket(self):
-        if (self.packet == "default_packet"):
-            return False
-        else:
-            self.content = self.packet
-            return True
+    def GetReceiver(self, string):
+        self.receiver = string
 
-    def GetPacket(self, string):
-        if(string == ""):
-            return False
-        self.packet = string
-        self.DividePacket()
+    def GetSender(self, string):
+        self.sender = string
+
+    def GetContent(self, string):
+        self.content = string
 
     def Receiver(self):
         return self.receiver
